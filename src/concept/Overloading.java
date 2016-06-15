1971914d14b06e8e5e6e682537e815853c907185
mayank <mayank283@live.com>
2016-06-16 01:42:16 +0530

    6/16/2016

---
+++ src/concept/Overloading.java
@@ -0,0 +1,18 @@
+package concept;
+
+/**
+ * Based on Automatic promotion or implicit type casting
+ * char(2 byte)->int(4 byte)->long(8 byte)->float(4 byte)->double(8 byte)
+ * byte(1)->short(2)->int(4)->long(8)->float(4)->double(8) 
+ * String->Object 
+ * between int and int... method int will get priority and int... has least priority */
+
+
+public class Overloading {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
