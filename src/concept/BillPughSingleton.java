04793926413050e1753fc8dcb5a074346f27cda0
Agarwal <mayank.f.agarwal@accenture.com>
2016-06-16 18:59:42 +0530

    6/16/2016

---
+++ src/concept/BillPughSingleton.java
@@ -0,0 +1,16 @@
+package concept;
+
+/**Most widely used Method to create a singleton class*/
+
+public class BillPughSingleton {
+
+    private BillPughSingleton(){}
+    
+    private static class SingletonHelper{
+        private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+    }
+    
+    public static BillPughSingleton getInstance(){
+        return SingletonHelper.INSTANCE;
+    }
+}
