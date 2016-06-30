76d6d362d70363c62bd8e6c2e1e8abb2be5815c0
mayank <mayank283@live.com>
2016-07-01 02:50:06 +0530

    7/1/2016

---
+++ src/tree/BinaryTreeNode.java
@@ -0,0 +1,32 @@
+package tree;
+
+public class BinaryTreeNode {
+	BinaryTreeNode left;
+	BinaryTreeNode right;
+	int data;
+
+	public BinaryTreeNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(BinaryTreeNode left) {
+		this.left = left;
+	}
+
+	public BinaryTreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(BinaryTreeNode right) {
+		this.right = right;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+}
