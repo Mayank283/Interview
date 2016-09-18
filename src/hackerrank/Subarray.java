10a1993f0e3f2bc66024b0112c87c4a268c18fa5
mayank <mayank283@live.com>
2016-09-18 20:54:56 +0530

    9/18/2016

---
+++ src/hackerrank/Subarray.java
@@ -0,0 +1,33 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Subarray {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] array = new int[n];
+		int count = 0;
+		for (int i = 0; i < array.length; i++) {
+			array[i] = in.nextInt();
+		}
+		in.close();
+		for (int i = 0; i <= array.length; i++) {
+			
+			for (int j = 0; j < array.length - i; j++) {
+				
+				int l = j, k = 0, sum = 0;
+				
+				while (k++ <= i) {
+					
+					sum = sum + array[l++];
+				}
+		
+				if (sum < 0)
+					count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
