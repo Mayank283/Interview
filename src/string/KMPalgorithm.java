e71f407abf21329192249818280d99e1e5fb1e95
mayank <mayank283@live.com>
2016-10-04 00:44:24 +0530

    10/4/2016

---
+++ src/string/KMPalgorithm.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package string;
+
+/**
+ * @author Mayank
+ *
+ */
+public class KMPalgorithm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/string/Tries.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package string;
+
+/**
+ * @author Mayank
+ *
+ */
+public class Tries {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
