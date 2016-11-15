e80feab5da3d4a8010f15d263c5313b8a19b5599
Agarwal <mayank.f.agarwal@accenture.com>
2016-11-15 17:48:38 +0530

    11/15/2016

---
+++ src/graph/AdjMatrixGraphTest.java
@@ -0,0 +1,19 @@
+package graph;
+
+public class AdjMatrixGraphTest {
+
+	public static void main(String[] args) {
+		AdjMatrixGraph graph = new AdjMatrixGraph(4);
+		graph.addVertex("Mathura");
+		graph.addVertex("Agra");
+		graph.addVertex("Delhi");
+		graph.addVertex("Jaipur");
+		graph.addEdge("Mathura", "Agra");
+		graph.addEdge("Mathura", "Delhi");
+		graph.addEdge("Delhi", "Jaipur");
+		graph.addEdge("Agra", "Jaipur");
+		System.out.println(graph.isConnected("Mathura", "Jaipur"));
+		System.out.println(graph.isConnected("Mathura", "Delhi"));
+		System.out.println(graph.getNeighbours("Jaipur"));
+	}
+}
